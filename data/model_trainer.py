e4eb151e1e63a225f8c1a4e20e79ea9dcc5e87c0
miraants <100205632+miraants@users.noreply.github.com>
2025-07-30 15:59:18 +0300

    Update model_trainer.py

--- data/model_trainer.py
+++ data/model_trainer.py
@@ -1,7 +1,1 @@
-import pickle
-from sentence_transformers import SentenceTransformer
 
-def train_and_save_model():
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-    with open("data/model.pkl", "wb") as f:
-        pickle.dump(model, f)
